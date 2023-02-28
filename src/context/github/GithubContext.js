0a049e49716066afa31127686d4fde14b7290a49
SilvesterSpath <spath.sz@gmail.com>
2023-02-28 05:40:50 +0100

    clear users from state

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,12 +9,14 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
+  /*   // Get initial users (testing purposes)
   const fetchUsers = async () => {
+    setLoading();
     const res = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -27,11 +29,49 @@ export const GithubProvider = ({ children }) => {
       type: 'GET_USERS',
       payload: data,
     });
+  }; */
+
+  // Get search results
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    // this is destructuring the data.items
+    const { items } = await res.json();
+
+    dispatch({
+      type: 'GET_USERS',
+      payload: items,
+    });
+  };
+
+  // Set loading
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
+
+  // Reset users
+  const resetUsers = () => {
+    dispatch({
+      type: 'RESET_USERS',
+    });
   };
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        resetUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
